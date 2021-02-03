6059ea0b14130c9940b2e426fe7614b80392a3da
Houston Santos <houston_santos@hotmail.com>
2021-02-03 17:55:01 -0300

    Add install packages

---
+++ InstallPackages.r
@@ -0,0 +1,33 @@
+# Instalação de pacotes
+
+install.packages(c(
+  'rodbc',
+  'dplyr',
+  'devtools',
+  'foreign',
+  'ggplot2',
+  'e1071',
+  'corrplot',
+  'caret',
+  'outliers',
+  'rpostgresql',
+  'forecast',
+  'mongolite',
+  'party',
+  'FSelector',
+  'class',
+  'randomForest',
+  'arules',
+  'arulesViz',
+  'PRROC',
+  'adabag',
+  'fpc',
+  'readxl',
+  'neuralnet',
+  'h2o',
+  'igraph',
+  'igraphdata',
+  'tm',
+  'mltools',
+  'data.table'
+), dependencies = TRUE)
